38d7b96d283571a537abc10cd551c521364dac00
Max <makshostroyshko@gmail.com>
2019-01-18 19:17:08 +0200

    added log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -9,11 +10,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-      if @user.save
-        redirect_to @user
-      else
-        render 'new'
-      end
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome, #{@user.name}!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   root 'relatives#index'
 
+  get 'sessions/new'
+  get 'users/new'
+  get 'signup',      to: 'users#new'
+  post 'signup',     to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # get 'user/:id', to: 'users#show', as: 'user'
 
   resources :users
