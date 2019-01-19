82ed287387754e2f787f59636e745a5e41b70fe5
Max <makshostroyshko@gmail.com>
2019-01-19 19:08:53 +0200

    added style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Family_tree"
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
+  end
 end
