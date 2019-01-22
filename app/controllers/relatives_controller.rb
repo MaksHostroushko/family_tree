05d27616f91ff6b603a1c98316d6448cada41021
Max <makshostroyshko@gmail.com>
2019-01-22 21:22:30 +0200

    fixed search && filters with pagintaion

--- app/controllers/relatives_controller.rb
+++ app/controllers/relatives_controller.rb
@@ -4,11 +4,12 @@ class RelativesController < ApplicationController
   def index
     @categories = Category.all
     @relatives = Relative.all
-    @relatives = @relatives.where(id: CategoryRelative.where(category_id: params[:filter]).pluck(:relative_id)) if params[:filter].present?
+    # @relatives = @relatives.where(id: CategoryRelative.where(category_id: params[:filter]).pluck(:relative_id)) if params[:filter].present?
     @user = current_user
     # @user = User.find(params[:id])
-    @relatives = Relative.search(params[:search]).order(created_at: :asc)  if params[:search]
-    @relatives = Relative.paginate(page: params[:page], per_page: 9)
+    # @relatives = Relative.search(params[:search]).order(created_at: :asc)  if params[:search]
+    @relatives = Relative.paginate(page: params[:page], per_page: 9).search(params[:search])
+    @relatives = Relative.paginate(page: params[:page], per_page: 9).where(id: CategoryRelative.where(category_id: params[:filter]).pluck(:relative_id)) if params[:filter].present?
 
     respond_to do |format|
       format.html
