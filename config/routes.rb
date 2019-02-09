1f6778d92492d524a26d0a06e407a011f0249917
Max <makshostroyshko@gmail.com>
2019-02-09 13:19:23 +0200

    added tests for realtive_controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,6 @@ Rails.application.routes.draw do
     get    '/login',   to: 'sessions#new'
     post   '/login',   to: 'sessions#create'
     delete '/logout',  to: 'sessions#destroy'
-    # get '/users',      to: 'users#index'
-    # get 'user/:id', to: 'users#show', as: 'user'
 
     resources :users
     resources :relatives do
