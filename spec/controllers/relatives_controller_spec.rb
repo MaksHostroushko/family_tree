4e77b83e4ae493e2e3839c6fd5262abbb0d979a9
Max <makshostroyshko@gmail.com>
2019-02-09 13:53:42 +0200

    added before_action tests

--- spec/controllers/relatives_controller_spec.rb
+++ spec/controllers/relatives_controller_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe RelativesController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  describe "#before_action " do
+    it { should use_before_action(:find_relative) }
+    it { should use_before_action(:correct_user) }
+    it { should_not use_before_action(:relative_params) }
+    it { should_not use_before_action(:find_user) }
+  end
 end
