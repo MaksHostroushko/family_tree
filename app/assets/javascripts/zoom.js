70533506d5d2c350136863c4739914c92339b149
MaksGostroushko <makshostroyshko@gmail.com>
2019-05-09 11:35:00 +0300

    fixed zoom.js

--- app/assets/javascripts/zoom.js
+++ app/assets/javascripts/zoom.js
@@ -2,16 +2,20 @@ $(document).on('turbolinks:load', function checkZoom() {
   let browserZoomLevel = Math.round(window.devicePixelRatio * 100);
 
   if (browserZoomLevel >= 90 && browserZoomLevel <= 100) {
-    $('#search').removeClass("ui-autocomplete-input");
+    // $('#search').removeClass("ui-autocomplete-input");
     $('#search').addClass("ui-autocomplete-input-2");
+    $('#search').removeClass("ui-autocomplete-input-3");
+    $('#search').removeClass("ui-autocomplete-input-4");
     $('#navbarDropdown').removeClass("nav_name_2");
   } else if (browserZoomLevel > 100 && browserZoomLevel <= 110) {
     $('#search').removeClass("ui-autocomplete-input-2");
+    $('#search').removeClass("ui-autocomplete-input-4");
     $('#search').addClass("ui-autocomplete-input-3");
     $('#navbarDropdown').removeClass("nav_name");
     $('#navbarDropdown').removeClass("nav_name_2");
   } else if (browserZoomLevel > 110) {
     $('#search').removeClass("ui-autocomplete-input-3");
+    $('#search').removeClass("ui-autocomplete-input-2");
     $('#search').addClass("ui-autocomplete-input-4");
     $('#navbarDropdown').removeClass("nav_name");
     $('#navbarDropdown').addClass("nav_name_2");
