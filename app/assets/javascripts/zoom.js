b2da3837d477163b0710a9bac5250fd2132b3525
Maxim Gostroushko <makshostroyshko@gmail.com>
2019-05-09 10:17:12 +0300

    some changes

--- app/assets/javascripts/zoom.js
+++ app/assets/javascripts/zoom.js
@@ -4,12 +4,17 @@ $(document).on('turbolinks:load', function checkZoom() {
   if (browserZoomLevel >= 90 && browserZoomLevel <= 100) {
     $('#search').removeClass("ui-autocomplete-input");
     $('#search').addClass("ui-autocomplete-input-2");
+    $('#navbarDropdown').removeClass("nav_name_2");
   } else if (browserZoomLevel > 100 && browserZoomLevel <= 110) {
     $('#search').removeClass("ui-autocomplete-input-2");
     $('#search').addClass("ui-autocomplete-input-3");
+    $('#navbarDropdown').removeClass("nav_name");
+    $('#navbarDropdown').removeClass("nav_name_2");
   } else if (browserZoomLevel > 110) {
     $('#search').removeClass("ui-autocomplete-input-3");
     $('#search').addClass("ui-autocomplete-input-4");
+    $('#navbarDropdown').removeClass("nav_name");
+    $('#navbarDropdown').addClass("nav_name_2");
   }
   setInterval(checkZoom, 100);
   });
