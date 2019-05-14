0f6ea31d429f24654d279592307c1e29a9400920
MaksGostroushko <makshostroyshko@gmail.com>
2019-05-14 15:22:53 +0300

    improve flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,4 +30,19 @@ module ApplicationHelper
       "#{current_year}"
     end
   end
+
+  def devise_error_messages!
+   return '' if resource.errors.empty?
+
+   messages = resource.errors.full_messages.map do |msg|
+     content_tag(:li, msg)
+   end .join
+   html <<-HTML
+   <div class="alert alert-error allert-danger">
+     <a href='#' class='close' data-dismiss='alert' &#215;></a>
+     <%= content_tag :div, msg if msg.is_a?(String) %>
+   </div>
+   HTML
+   html.html_safe
+ end
 end
