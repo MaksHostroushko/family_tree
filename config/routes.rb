7b910cccb79340b288ec96a4a172d6daf840bc13
MaksGostroushko <makshostroyshko@gmail.com>
2019-05-14 17:55:43 +0300

    fixed devise && omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   ActiveAdmin.routes(self)
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
