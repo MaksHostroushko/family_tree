a9d90a5388e294df2bbce6863a661efe7d2a4245
MaksGostroushko <makshostroyshko@gmail.com>
2019-05-14 18:14:01 +0300

    some changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'makshostroyshko@gmail.com', password: 'qwerty', password_confirmation: 'qwerty') if Rails.env.development?
+
+10.times do
+  Relative.create!(first_name: Faker::Name.first_name, second_name: Faker::Name.last_name , born: '05/14/1998', description: Faker::TvShows::Simpsons.quote, user_id: 10)
+end
+
+20.times do
+  User.create!(name: Faker::Name.first_name, second_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'qwerty', password_confirmation: 'qwerty', provider: nil, uid: nil)
+end
